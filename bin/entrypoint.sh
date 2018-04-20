d3be6409b4610ce6c702b86939f591f9cc832fa2
Ben Hohnke <settra@gmail.com>
2018-04-20 12:18:06 +1000

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /devpi-server/server-root && 
 devpi-server --init --serverdir /devpi-server/server-root && 
-devpi-server --start --serverdir /devpi-server/server-root --host 0.0.0.0
+devpi-server --serverdir /devpi-server/server-root --host 0.0.0.0
